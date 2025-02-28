0180a78a16eaccaf4f0a919eb477d21c8e9f176b
Marta Paz <pazmarta@gmail.com>
2025-02-28 10:24:07 +0000

    add tab button event

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,9 +1,14 @@
 import './TabButton.css'
 
 export default function TabButton({children}) {
+  function handleClick() {
+    console.log('Hello World')
+  }
+
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={handleClick}>{children}</button>
     </li>
   )
 }
+
